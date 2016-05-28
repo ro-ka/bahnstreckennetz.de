148b2d17d85c6019d413a630cc0da1ae04c5fb85
Robert Katzki <robert@katzki.de>
2016-05-28 08:42:33 +0200

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,16 @@ APP_NAME="bahnstreckennetz.de"
 SSH_HOST="robert@46.101.218.230"
 APP_DIR="/var/www/$APP_NAME"
 
-# BUNDLE_CMD="tar -czf bundle.tgz ./"
-BUNDLE_CMD="tar -czf bundle.tgz index.html"
+BUNDLE_CMD="tar -czf bundle.tgz ./"
 
-# echo Building...
-# grunt build
+echo Building...
+npm run build
 echo Deploying...
-# pushd "./dist"
+pushd "./build"
 $BUNDLE_CMD > /dev/null 2>&1 &&
-# popd
-scp "./bundle.tgz" $SSH_HOST:/tmp/ > /dev/null 2>&1 &&
-rm "./bundle.tgz" > /dev/null 2>&1 &&
+popd
+scp "./build/bundle.tgz" $SSH_HOST:/tmp/ > /dev/null 2>&1 &&
+rm "./build/bundle.tgz" > /dev/null 2>&1 &&
 ssh $SSH_HOST 'bash -s' > /dev/null 2>&1 <<ENDSSH
 if [ ! -d "$APP_DIR" ]; then
   mkdir -p $APP_DIR
